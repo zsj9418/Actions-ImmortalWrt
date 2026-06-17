ced450574fd1414d4d0f8dae26d76341aafd391e
zsj9418 <34057614+zsj9418@users.noreply.github.com>
2026-06-17 22:08:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,27 +10,13 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-#echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
-#echo "src-git customsd https://github.com/Ljzkirito/smartdns-openwrt.git;Release46-conf" >> "feeds.conf.default"
-
-# Replace /lib/netifd/ppp-down
-# https://github.com/hanwckf/immortalwrt-mt798x/issues/57
-#cat >> package/network/services/ppp/files/lib/netifd/ppp-down <<EOF
-#while true; do
-#  if ubus -S list "network.interface.wan_6"; then
-#    # ubus call network.interface.wan_6 down
-#    logger "waiting wan_6 down..."
-#    sleep 1
-#  else
-#    break
-#  fi
-#done
-#EOF
-# 取消Dnsmasq缓存，由smartdns负责。不修改ttl
-#sed -i 's/cachesize\t8000/cachesize\t0/g' package/network/services/dnsmasq/files/dhcp.conf
-#sed -i 's/mini_ttl\t3600/mini_ttl\t0/g' package/network/services/dnsmasq/files/dhcp.conf
-
 # Remove 6in4
 sed -i 's/ +6in4//g' package/emortal/ipv6-helper/Makefile
 sed -i '/hotplug.d/d' package/emortal/ipv6-helper/Makefile
 rm -fv package/emortal/ipv6-helper/files/60-6in4
+
+# Add luci-app-pushbot feed
+echo "src-git pushbot https://github.com/zzsj0928/luci-app-pushbot.git;master" >> "feeds.conf.default"
+
+# Add daed feed
+echo "src-git daed https://github.com/QiuSimons/luci-app-daed.git;master" >> "feeds.conf.default"
